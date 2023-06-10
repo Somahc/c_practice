75380922bacf0907ed427d761a34dc4fa896f979
Soma Sekine <s.sekine.howcollect@gmail.com>
2023-06-10 21:14:02 +0900

    2_d, 3_a

--- itp1_2_d.cpp
+++ itp1_2_d.cpp
@@ -4,19 +4,26 @@ int main() {
     int w, h, x, y, r;
     std::cin >> w >> h >> x >> y >> r;
 
-    bool isYoko, isTate;
-    isYoko = isTate = false;
+    if (0 <= (x-r) and (x+r) <= w and 0 <= (y-r) and (y+r) <= h) {
+        std::cout << "Yes" << std::endl;
+    }else{
+        std::cout << "No" << std::endl;
 
-    if (0<x-r && w>x+r) {
-        isYoko = true;
-    }
-    if (0<y-r && h>y+r) {
-        isTate = true;
     }
 
-    if(isYoko && isTate) {
-        std::cout << "Yes" << "\n";
-    } else {
-        std::cout << "No" << "\n";
-    }
+    // bool isYoko, isTate;
+    // isYoko = isTate = false;
+
+    // if (0<x-r && w>x+r) {
+    //     isYoko = true;
+    // }
+    // if (0<y-r && h>y+r) {
+    //     isTate = true;
+    // }
+
+    // if(isYoko && isTate) {
+    //     std::cout << "Yes" << "\n";
+    // } else {
+    //     std::cout << "No" << "\n";
+    // }
 }
