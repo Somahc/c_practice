c09d4f65c42ef710b247af133f17181078b6918f
Soma Sekine <s.sekine.howcollect@gmail.com>
2023-07-11 11:56:46 +0900

    305a

---
+++ ABC305/a.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+
+    if(N % 5 == 0) cout << N << endl;
+    else {
+        if(N % 5 >= 3) {
+            cout << N + (5 - (N % 5)) << endl;
+        } else {
+            cout << N - (N % 5) << endl;
+        }
+    }
+}
