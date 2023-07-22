41e7bb1a5d6354e31d5147f2d5d62c0e39cd4d2d
Soma Sekine <s.sekine.howcollect@gmail.com>
2023-07-22 23:30:16 +0900

    301a

---
+++ ABC301/a.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n, t=0, a=0;
+    string s;
+    cin >> n >> s;
+
+    for(int i=0; i<n; i++) {
+        if (s[i] == 'T') t++;
+        else a++;
+    }
+
+    if(t > a) cout << "T" << endl;
+    else if(t < a) cout << "A" << endl;
+    // else if(s[n] == 'T') cout << 'A' << endl;
+    // else cout << 'T' << endl;
+    else cout << char('T' + 'A' - s.back()) << endl;
+}
