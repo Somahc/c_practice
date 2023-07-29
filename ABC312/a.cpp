1e4b2a2bf276645687c91c7ef8d5211e1233e632
Soma Sekine <s.sekine.howcollect@gmail.com>
2023-07-29 21:16:49 +0900

    312a

---
+++ ABC312/a.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string S;
+    cin >> S;
+    if(S == "ACE") cout << "Yes" << endl;
+    else if(S == "BDF") cout << "Yes" << endl;
+    else if(S == "CEG") cout << "Yes" << endl;
+    else if(S == "DFA") cout << "Yes" << endl;
+    else if(S == "EGB") cout << "Yes" << endl;
+    else if(S == "FAC") cout << "Yes" << endl;
+    else if(S == "GBD") cout << "Yes" << endl;
+    else cout << "No" << endl;
+}
