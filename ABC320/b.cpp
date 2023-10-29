ad6d375895426976b40c212d240bd8c5b58e07a1
Soma Sekine <a8121151@aoyama.jp>
2023-10-28 23:52:30 -0400

    320b_kaisetu

---
+++ ABC320/b.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i=0; i<(n); i++)
+using namespace std;
+
+bool is_palindrome(string s){
+    int n = s.size();
+    for(int i=0; i<n; i++) if(s[i] != s[n-1-i]) return false;
+    return true;
+}
+
+int main(){
+    string s;
+    cin >> s;
+    int n = s.size();
+    int ans = 1;
+    for(int i=0; i<n; i++){
+        for(int j = i+1; j<=n; j++){
+            string t = "";
+            for(int k=i; k<j; k++) t += s[k];
+            if (is_palindrome(t)) ans = max(ans, j-i);
+        }
+    }
+    cout << ans << endl;
+    return 0;
+}
