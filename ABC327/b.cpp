47558048ac4095665c25ccf6f9fa9ed160fb50fa
Soma Sekine <a8121151@aoyama.jp>
2023-11-04 18:14:15 -0400

    327b

--- ABC327/b.cpp
+++ ABC327/b.cpp
@@ -2,37 +2,16 @@
 #define rep(i, n) for (int i=0; i<(n); i++)
 using namespace std;
 
-long long int fastExponentiation(long long int base, int exponent) {
-    long long int result = 1;
-    while (exponent > 0) {
-        if (exponent % 2 == 1) {
-            result *= base;
-        }
-        base *= base;
-        exponent /= 2;
-    }
-    return result;
-}
-
-int main() {
-    long long int b;
+int main(){
+    long long b;
     cin >> b;
-
-    long long int left = 0, right = b;
-    while (left <= right) {
-        long long int mid = left + (right - left) / 2;
-        long long int tmp = fastExponentiation(mid, mid);
-
-        if (tmp == b) {
-            cout << mid << endl;
+    for(int a = 1; a <= 15; a++){
+        long long x = 1;
+        for(int i = 0; i < a; i++) x *= a;
+        if(x == b){
+            cout << a << endl;
             return 0;
-        } else if (tmp < b) {
-            left = mid + 1;
-        } else {
-            right = mid - 1;
         }
     }
-
     cout << "-1" << endl;
-    return 0;
 }
