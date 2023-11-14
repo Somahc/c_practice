a34e53e244df7735332ed59267edee37a89d96d0
Soma Sekine <a8121151@aoyama.jp>
2023-11-14 17:56:28 -0500

    291c

--- abc291/c/main.cpp
+++ abc291/c/main.cpp
@@ -3,6 +3,24 @@
 using namespace std;
 
 int main(){
-    int n, m;
-    cin >> n >> m;
+    int n, x=0, y=0;
+    string s;
+    cin >> n >> s;
+    set<pair<int,int>> visited({{0,0}});
+
+    for(auto c:s){
+        if(c == 'L') x--;
+        if(c == 'R') x++;
+        if(c == 'U') y++;
+        if(c == 'D') y--;
+
+        if(visited.find({x,y}) != visited.end()){
+            cout << "Yes" << endl;
+            return 0;
+        }
+        visited.insert({x,y});
+    }
+
+    cout << "No" << endl;
+    return 0;
 }
