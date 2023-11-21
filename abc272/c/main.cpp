dd1d550fa8adba2bfd0aeb6c28bbf3a95363a3dc
Soma Sekine <a8121151@aoyama.jp>
2023-11-20 22:52:13 -0500

    272c

--- abc272/c/main.cpp
+++ abc272/c/main.cpp
@@ -1,8 +1,25 @@
 #include <bits/stdc++.h>
 #define rep(i, n) for (int i=0; i<(n); i++)
 using namespace std;
+using ll  = long long;
 
 int main(){
-    int n, m;
-    cin >> n >> m;
+    int n;
+    cin >> n;
+    vector<ll> a(n), odd(n,0), even(n,0);
+    rep(i,n) {
+        cin >> a[i];
+        a[i] % 2 == 0 ? even.emplace_back(a[i]) : odd.emplace_back(a[i]);
+    }
+    sort(odd.begin(), odd.end(), greater<ll>());
+    sort(even.begin(), even.end(), greater<ll>());
+    // cout << even[0] + even[1] << endl;
+    if(odd.size() < 2 and even.size() < 2) {
+        ll ans = a[0] + a[1];
+        ans % 2 == 1 ? cout << "-1" << endl : cout << ans << endl;
+        return 0;
+    }
+    ll ans = max(odd[0] + odd[1], even[0] + even[1]);
+
+    ans % 2 == 1 ? cout << "-1" << endl : cout << ans << endl;
 }
