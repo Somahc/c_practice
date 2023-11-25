f905ae095bedc5ec64e2b4684e695355ca9d1067
Soma Sekine <a8121151@aoyama.jp>
2023-11-24 23:20:48 -0500

    265c

--- abc265/c/main.cpp
+++ abc265/c/main.cpp
@@ -3,6 +3,27 @@
 using namespace std;
 
 int main(){
-    int n, m;
-    cin >> n >> m;
+    int h, w;
+    cin >> h >> w;
+
+    vector<string> g(h);
+    for(auto&s : g) cin >> s;
+    vector vis(h, vector(w, false));
+
+    int i=0, j=0;
+    while(1){
+        if(vis[i][j] == true){
+            cout << "-1" << endl;
+            return 0;
+        }
+        vis[i][j] = true;
+        if(g[i][j] == 'R' and j != w-1) j++;
+        else if(g[i][j] == 'L' and j != 0) j--;
+        else if(g[i][j] == 'U' and i != 0) i--;
+        else if(g[i][j] == 'D' and i != h-1) i++;
+        else {
+            cout << i+1 << " " << j+1 << endl;
+            return 0;
+        }
+    }
 }
