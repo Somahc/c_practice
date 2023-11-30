5dce453eed15ffb85ab5a627c4f6e7793bd2f00f
Soma Sekine <a8121151@aoyama.jp>
2023-11-30 10:37:16 -0500

    276c

---
+++ abc276/c.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i=0; i<(n); i++)
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    vector<int> p(n);
+    for(int i = 0; i < n; i++){
+        cin >> p[i];
+    }
+    prev_permutation(p.begin(), p.end());
+    for(int i = 0; i < n; i++){
+        cout << p[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
