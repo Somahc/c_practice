67d69475fddaf6a6bf02e3c95678171a4ca2ab97
Soma Sekine <a8121151@aoyama.jp>
2023-12-02 10:25:01 -0500

    midoriika_b

---
+++ midoriika_B.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i=0; i<(n); i++)
+using namespace std;
+
+int main(){
+    int A, B, a, b;
+    cin >> A >> B >> a >> b;
+    int cnt = 0;
+    while(true){
+        if((A*cnt + (a-b)) % B == 0) {
+            cout << A*cnt + a << endl;
+            return 0;
+        }
+        cnt++;
+    }
+
+    return 0;
+}
