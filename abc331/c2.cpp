9593f35dedde1c8fcace1f3bb5053acc2cd28f88
Soma Sekine <a8121151@aoyama.jp>
2023-12-03 00:34:03 -0500

    321c

--- abc331/c2.cpp
+++ abc331/c2.cpp
@@ -4,41 +4,25 @@ using namespace std;
 using ll  = long long;
 
 int main(){
-    ll n;
+    int n;
     cin >> n;
-    vector<ll> a(n);
-    map<ll, ll> mp;
-    map<ll, ll> mp2;
-    set<ll> st;
+    vector<int> a(n);
+    map<int, vector<int>> mp;
 
-    rep(i, n){
+    for(int i = 0; i < n; i++){
         cin >> a[i];
-        mp[a[i]]++;
-        st.insert(a[i]);
+        mp[a[i]].push_back(i);
     }
 
-    for(auto it = st.begin(); it != st.end(); ++it){
-        ll x = *it;
-        ll tmp = 0;
-        for(auto y_it = it; y_it != st.end(); ++y_it){
-            ll y = *y_it;
-            if(y > x){
-                tmp += y * mp[y];
-            }
-        }
-        mp2[x] = tmp;
+    long long int wa = accumulate(a.begin(), a.end(), 0LL);
+
+    vector<long long> ans(n);
+    for(auto [v,i_list] : mp){
+        wa-=(long long)v*i_list.size();
+        for(auto i:i_list)ans[i] = wa;
     }
 
-    for(auto x : a){
-        auto it = st.lower_bound(x);
-        if (it != st.begin()) {
-            --it;
-            cout << mp2[*it] << " ";
-        } else {
-            cout << 0 << " ";
-        }
-    }
-    cout << endl;
+    for(auto x:ans)cout << x << ' ';
 
     return 0;
 }
