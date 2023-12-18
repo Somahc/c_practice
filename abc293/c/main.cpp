cbcf958eee0939b8c9747ef03428f9da32ed9d9a
Soma Sekine <a8121151@aoyama.jp>
2023-12-18 12:31:05 -0500

    293c

--- abc293/c/main.cpp
+++ abc293/c/main.cpp
@@ -3,6 +3,33 @@
 using namespace std;
 
 int main(){
-    int n, m;
-    cin >> n >> m;
+    int h, w;
+    cin >> h >> w;
+    int a[11][11];
+    for(int y = 1; y <= h; y++){
+        for(int x = 1; x <= w; x++){
+            cin >> a[x][y];
+        }
+    }
+
+    int p[20], l = h + w - 2, ans = 0;
+    for(int i = 1; i <= l; i++){
+        if(i > w-1) p[i] = 1;
+        else p[i] = 0;
+    }
+
+    do{
+        int x=1, y=1;
+        set<int> st; st.insert(a[1][1]);
+        for(int i = 1; i <= l; i++){
+            if(p[i]) y++;
+            else x++;
+
+            st.insert(a[x][y]);
+        }
+        if(st.size() == l+1) ans++;
+    }while(next_permutation(p+1, p+1+l));
+
+    cout << ans << endl;
+    return 0;
 }
