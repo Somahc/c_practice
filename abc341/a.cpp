dd870c8fe2a59d516e7f00d765df5c712045262c
Soma Sekine <a8121151@aoyama.jp>
2024-04-12 23:20:48 -0400

    341a

---
+++ abc341/a.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (n); i++)
+using namespace std;
+using ll = long long;
+
+int main()
+{
+    int n;
+    cin >> n;
+    rep(i, n)
+    {
+        cout << "10";
+    }
+    cout << "1" << endl;
+    return 0;
+}
