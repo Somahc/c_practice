17d000603d612a16d50780d861a4fd9fa4be676d
Soma Sekine <a8121151@aoyama.jp>
2024-05-11 23:51:18 +0900

    239b

---
+++ abc239/b.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (n); i++)
+using namespace std;
+using ll = long long;
+int main()
+{
+    ll x;
+    cin >> x;
+    if (x % 10 == 0)
+    {
+        cout << x / 10 << endl;
+        return 0;
+    }
+
+    if (x > 0)
+    {
+        cout << ((x / 10) * 10) / 10 << endl;
+    }
+    else
+    {
+        cout << (((x / 10) * 10) / 10) - 1 << endl;
+    }
+    return 0;
+}
