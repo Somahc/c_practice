8defdaeba2677e16644ad45f112e8142ca15a188
Soma Sekine <a8121151@aoyama.jp>
2025-01-24 16:04:23 +0900

    363a

---
+++ abc363/a.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (n); i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+    int r;
+    cin >> r;
+
+    cout << 100 - (r % 100) << endl;
+    return 0;
+
+}
