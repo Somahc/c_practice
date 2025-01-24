f450c58acabc6f6dd56d6376f1e77904a3693b61
Soma Sekine <a8121151@aoyama.jp>
2025-01-24 16:39:02 +0900

    364a

---
+++ abc364/a.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (n); i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+    int n;
+    cin >> n;
+    string currentDish;
+    bool isPreviousSweet = false;
+
+    for (int i = 0; i < n; i++) {
+        cin >> currentDish;
+        if (currentDish == "sweet") {
+            if (isPreviousSweet && i != n - 1) {
+                cout << "No" << endl;
+                return 0;
+            }
+
+            isPreviousSweet = true;
+        } else {
+            isPreviousSweet = false;
+        }
+    }
+
+    cout << "Yes" << endl;
+    return 0;
+
+}
